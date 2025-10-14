9e072bc823e939dc2f8961c21fd4b5b55dfb136e
Myth <muhammad.j1441@gmail.com>
2025-10-14 14:47:36 +0100

    Add files via upload

--- my_first_app.py
+++ my_first_app.py
@@ -1,15 +1,16 @@
 from tkinter import *
 
+
+
 def buttonClicked():
-	global e1, e2
-	text = e1.get()
-	print(e1 + e2)
-	add.config(text = f"+")
+	global text, text2
+	text = int(e1.get())
+	text2 = int(e2.get())
+	answer.config(text = f"{text}+{text2}={text+text2}")
+
 	
 	
-
-
-
+	
 myFirstApp = Tk()
 myFirstApp.geometry("800x800")
 myFirstApp.title("Addition Calculator")
@@ -21,22 +22,30 @@ e1 = Entry(myFirstApp, width = 30)
 e1.pack()
 
 
-b1 = Button(myFirstApp, text = "OK", command = buttonClicked,
-font = ("Calibri", 50))
-b1.pack()
+#b1 = Button(myFirstApp, text = "OK", command = buttonClicked,
+#font = ("Calibri", 50))
+#b1.pack()
 
 
-add = Label(myFirstApp,text = "",
+add = Label(myFirstApp,text = "+",
 	font = ("Arial", 100))
 add.pack()
 
+test = Label(myFirstApp, text = "Enter another number",
+	font = ("Arial", 17))
+test.pack()
 
 e2 = Entry(myFirstApp, width = 30)
 e2.pack()
 
-b2= Button(myFirstApp, text = "OK", command = buttonClicked,
-font = ("Calibri", 50))
+b2= Button(myFirstApp, text = "ANSWER", command = buttonClicked,
+font = ("Calibri", 30))
 b2.pack()
+
+
+answer = Label(myFirstApp, text = "",
+	font = ("Arial", 30))
+answer.pack()
 
 
 
