5bdbaa09e9f0d74bd1aa240c00939e46df431342
Myth <muhammad.j1441@gmail.com>
2025-11-18 22:23:14 +0000

    Connect4.py
    Connect4
    so far:
    - made the frame and holes for the counters
    - Currently trying to go through the array and place a corresponding counter colour in the frame.

--- Connect4.py
+++ Connect4.py
@@ -1,19 +1,18 @@
-board = [["r","w","y","r","r","r","w","y","y"],
-		["w","w","y","r","r","r","y","y","w"],
-		["r","y","y","w","r","r","w","y","w"],
-		["w","y","y","w","r","r","w","y","y"],
-		["r","w","w","r","r","r","y","r","r"],
-		["w","y","y","w","r","r","r","y","w"],
-		["y","w","y","w","r","r","w","y","y"],
-		["y","w","y","w","r","r","w","y","w"],
-		["w","w","y","y","r","r","w","y","r"]]
+board = [["r", "w", "y", "r", "r", "r", "w", "y", "y"],
+         ["w", "w", "y", "r", "r", "r", "y", "y", "w"],
+         ["r", "y", "y", "w", "r", "r", "w", "y", "w"],
+         ["w", "y", "y", "w", "r", "r", "w", "y", "y"],
+         ["r", "w", "w", "r", "r", "r", "y", "r", "r"],
+         ["w", "y", "y", "w", "r", "r", "r", "y", "w"],
+         ["y", "w", "y", "w", "r", "r", "w", "y", "y"],
+         ["y", "w", "y", "w", "r", "r", "w", "y", "w"],
+         ["w", "w", "y", "y", "r", "r", "w", "y", "r"]]
 
 
-row = 0
-col = 0
 
 from pygame import *
-init() # set up pygame
+
+init()  # set up pygame
 width = 700
 height = 700
 # surface! - can draw to it
@@ -24,44 +23,35 @@ endGame = False
 column_max = 9
 row_max = 8
 while endGame == False:
-	# events
-	for e in event.get(): # get all events
-		if e.type == QUIT:
-			endGame = True
-	screen.fill((0,0,0)) # tuple - RGB
-	draw.rect(screen, (0,0,255), (20,20,660,600))
-	y = 55
-	while y <= 55 + (65 * row_max):
-		x = 55
-		while x <= 55 + (65 * column_max):
-			draw.circle(screen, (255,255,255), (x,y), 30)
-			x = x + 65
-		y = y + 65
+    # events
+    for e in event.get():  # get all events
+        if e.type == QUIT:
+            endGame = True
+    screen.fill((0, 0, 0))  # tuple - RGB
+    draw.rect(screen, (0, 0, 255), (20, 20, 660, 600))
 
-r = (255,0,0)
-w =	(255,255,255)
-y =	(255,255,0)
+    y = 55
+    while y <= 55 + (65 * row_max):
+        x = 55
+        while x <= 55 + (65 * column_max):
+            draw.circle(screen, (255, 255, 255), (x, y), 30)
+            x = x + 65
+        y = y + 65
 
-while row < len(board): 
-	col = 0 
-	while col < len(board[0]):
-		if board == "r":
-				y = 55
-	while y <= 55 + (65 * row_max):
-		x = 55
-		while x <= 55 + (65 * column_max):
-			draw.circle(screen, (255,255,255), (x,y), 30)
-			x = x + 65
-		y = y + 65
-			
-			
-		col = col + 1
-	row = row + 1
-			
-			
-	
-	
-	
-	
-	display.flip()
-	# draw
+
+
+    row = 0
+    while row < len(board):
+        col = 0
+        while col < len(board[row]):
+            if board[row][col] == "r":
+                draw.circle(screen, (255, 0, 0), (x, y), 30)
+            elif board[row][col] == "y":
+                draw.circle(screen, (255, 255, 0), (x, y), 30)
+            elif board[row][col] == "w":
+                draw.circle(screen, (255, 255, 255), (x, y), 30)
+            col = col + 1
+        row = row + 1
+
+
+    display.flip()
